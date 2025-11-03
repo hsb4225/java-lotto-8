27f45106ee11006c045c5eee129ce36ce7616dd8
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2025-11-03 22:54:54 +0900

    test(LottoTest): 단위 테스트 클래스

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,21 +5,59 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 로또_번호_범위_검증() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("맞춘 번호 개수 확인")
+    @Test
+    void countMatch_테스트() {
+        Lotto l1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto l2 = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+        int match = l1.countMatch(l2);
+        assertThat(match).isEqualTo(3);
+    }
+
+    @DisplayName("특정 번호 포함 여부 확인")
+    @Test
+    void contains_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.contains(3)).isTrue();
+        assertThat(lotto.contains(7)).isFalse();
+    }
+
+    @DisplayName("오름차순 정렬 확인")
+    @Test
+    void 정렬_테스트() {
+        Lotto lotto = new Lotto(List.of(6, 2, 5, 3, 1, 4));
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
 }
