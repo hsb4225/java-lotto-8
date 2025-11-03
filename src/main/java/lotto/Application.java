723af5498b4983a5ec1fd672e628c95bba864cb0
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2025-11-03 22:40:19 +0900

    feat(Application): application main

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,23 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Input input = new Input();
+        Output output = new Output();
+        CreateLotto creator = new CreateLotto();
+
+        int purchaseMoney = input.readPurchaseMoney();
+        List<Lotto> myLottos = creator.lottoList(purchaseMoney);
+
+        output.printLottos(myLottos);
+        InputLotto winningLotto = input.readLotto();
+
+        Result result = new Result(myLottos, winningLotto, purchaseMoney);
+        output.printResult(result);
 
     }
 }
